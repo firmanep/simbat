f46e618dae3eccbd14898f0e30e0e6fd529725f6
Adnan <muh.adnansuryaazis@gmail.com>
2020-10-01 17:47:28 +0800

    add_data with get request

---
+++ api/add_data.php
@@ -0,0 +1,32 @@
+<?php
+
+
+function getTime($stamp){
+    $date = new DateTime("now", new DateTimeZone('Asia/Makassar') );
+    $waktu = $date->format('Y-m-d H:i:s');
+    // $timestamp = date_timestamp_get($date);
+    
+    return $waktu;        
+}
+
+
+
+
+$conn = mysqli_connect("localhost", "admin", "makassar", "simbat");
+if(isset($_GET['s1']) && isset($_GET['s2']) && isset($_GET['s3']) && isset($_GET['s4'])){
+    $sekarang = getTime(time());
+    $sql = "INSERT INTO pengukuran (sensor1, sensor2, sensor3, sensor4, waktu) 
+    VALUES (".$_GET['s1'].", ".$_GET['s2'].", ".$_GET['s3'].", ".$_GET['s4'].", '".$sekarang."')";
+    $result = mysqli_query($conn, $sql);
+    if(!($result)){
+        echo 'Error query pengukuran';
+    }else{
+        echo 'Berhasil';
+    }
+    echo $sql;
+}else{
+    echo 'Data tidak lengkap';
+}
+
+
+?>

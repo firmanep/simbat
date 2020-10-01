50c6bd5392624c3374a9bcb0b2630978d69ebf87
Adnan <muh.adnansuryaazis@gmail.com>
2020-10-01 17:10:26 +0800

    tes grafik

--- index.php
+++ index.php
@@ -23,13 +23,11 @@ include('partials/global.php');
                         <h1 class="mt-4">Static Navigation</h1>                        
                         <div class="card mb-4">
                             <div class="card-body">
-                                <p class="mb-0">
-                                    This page is an example of using static navigation. By removing the
-                                    <code>.sb-nav-fixed</code>
-                                    class from the
-                                    <code>body</code>
-                                    , the top navigation and side navigation will become static on scroll. Scroll down this page to see an example.
-                                </p>
+                                <div class="row">
+                                    <div class="col-12">
+                                        <canvas id="myChart" width="400" height="400"></canvas>
+                                    </div>
+                                </div>
                             </div>
                         </div>                       
                     </div>
@@ -38,5 +36,49 @@ include('partials/global.php');
             </div>
         </div>
         <?php include('partials/scripts.php'); ?>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+        <script>
+            var ctx = document.getElementById('myChart').getContext('2d');
+            var myLineChart = new Chart(ctx, {
+                type: 'line',
+                    data: {
+                    labels: [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050],
+                    datasets: [{ 
+                        data: [86,114,106,106,107,111,133,221,783,2478],
+                        label: "Africa",
+                        borderColor: "#3e95cd",
+                        fill: false
+                    }, { 
+                        data: [282,350,411,502,635,809,947,1402,3700,5267],
+                        label: "Asia",
+                        borderColor: "#8e5ea2",
+                        fill: false
+                    }, { 
+                        data: [168,170,178,190,203,276,408,547,675,734],
+                        label: "Europe",
+                        borderColor: "#3cba9f",
+                        fill: false
+                    }, { 
+                        data: [40,20,10,16,24,38,74,167,508,784],
+                        label: "Latin America",
+                        borderColor: "#e8c3b9",
+                        fill: false
+                    }, { 
+                        data: [6,3,2,2,7,26,82,172,312,433],
+                        label: "North America",
+                        borderColor: "#c45850",
+                        fill: false
+                    }
+                    ]
+                },
+                options: {
+                    title: {
+                    display: true,
+                    text: 'World population per region (in millions)'
+                    }
+                }
+                
+            });
+        </script>
     </body>
 </html>
